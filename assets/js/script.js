56ece51ec9116a2354a46db9a8c29a02c0eee2b6
Mishell-A <mishellarroyo2@gmail.com>
2024-09-30 20:20:45 -0500

    firebase link

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCc7hyr1k64BsRgv0n8ZU8fvKVj99rvo9U",
+    authDomain: "diario-digital-6e221.firebaseapp.com",
+    projectId: "diario-digital-6e221",
+    storageBucket: "diario-digital-6e221.appspot.com",
+    messagingSenderId: "432925276676",
+    appId: "1:432925276676:web:e5fbc379e1bc9e1251d351"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
